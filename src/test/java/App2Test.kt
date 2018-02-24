6f7b69f80a712c5c6a660cecb2161d86d6d01337
PrincessEve <princess.eve@outlook.co.th>
2018-02-24 11:31:50 +0700

    first build inside android studio

---
+++ src/test/java/App2Test.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by princ on 2/24/2018.
+ */
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
